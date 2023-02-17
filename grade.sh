f8688695515c32488b9101790c946af44f84944d
Taiki-id31 <122572036+Taiki-id31@users.noreply.github.com>
2023-02-16 19:11:33 -0800

    commit

--- grade.sh
+++ grade.sh
@@ -3,3 +3,28 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+cd student-submission
+if [[ -f ListExamples.java ]]
+then
+    echo "ListExamples found"
+else 
+    echo "ListExamples not found"
+    exit 1
+fi
+mkdir lib
+cp ../lib/hamcrest-core-1.3.jar ./lib/
+cp ../lib/junit-4.13.2.jar ./lib/
+cp ../TestListExamples.java ./
+
+javac ListExamples.java
+
+javac -cp $CPATH  TestListExamples.java 
+
+if [[ $? -ne 0 ]]
+then 
+    echo "compiler error"
+fi 
+
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+
